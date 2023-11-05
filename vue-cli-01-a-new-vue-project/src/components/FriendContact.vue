4f482defd68faab2c924d9286aa764812cc0fc24
Christian Nocerino <nocerino.christian.cn@gmail.com>
2023-11-05 18:02:50 +0100

    new project and props

---
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,47 @@
+<template>
+    <li>
+      <h2>{{ name }} {{ friendIsFavorite === '1' ? 'Favorite' : ''}}</h2>
+      <button @click="toggleFavorite"> Toogle Favorite </button>
+      <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+      <ul v-if="detailsAreVisible">
+        <li>
+          <strong>Phone:</strong>
+          {{ phoneNumber }}
+        </li>
+        <li>
+          <strong>Email:</strong>
+          {{ emailAddress }}
+        </li>
+      </ul>
+    </li>
+  </template>
+  
+  <script>
+  export default {
+    props: [
+      'name',
+      'phoneNumber',
+      'emailAddress',
+      'isFavorite'
+    ],
+    data() {
+      return {
+        detailsAreVisible: false,
+        /// per modificare il valore di un dato bisogna assegnarlo a una variabile interna 
+        friendIsFavorite: this.isFavorite
+      };
+    },
+    methods: {
+      toggleDetails() {
+        this.detailsAreVisible = !this.detailsAreVisible;
+      },
+      toggleFavorite() {
+        if(this.friendIsFavorite === '1') {
+          this.friendIsFavorite = '0';
+        } else {
+          this.friendIsFavorite = '1';
+        }
+      }
+    }
+  };
+  </script>
